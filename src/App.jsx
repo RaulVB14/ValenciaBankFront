70965c3122f46683af5fb9bb871ac5aa1064f9b3
rrvb9 <rrvb9674@gmail.com>
2024-08-21 12:07:10 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+import { Router, useHistory } from 'react-router-dom'
+import NavBar from "./components/NavBar";
+import Login from "./Login"
+
+export function App() {
+  // Función para cerrar la ventana pero no funciona del todo
+  const handleExit = () => {
+    window.close();
+  };
+
+  const goLogin = () => {
+    return(
+        <div className="">
+            <Router>
+                <NavBar/>
+                <Routes>
+                    <Route path="/Login" element={<Login/>}/>
+                </Routes>
+            </Router>
+        </div>
+
+    );
+  };
+
+  return (
+    <div>
+      <h1>HOLA</h1>
+      <h2>Adio</h2>
+      <div className="button-container">
+        <button className="btn" onClick={handleExit}>Salir</button>
+        <button className="btn" onClick={goLogin}>Login</button>
+        <button className="btn">Registro</button>
+      </div>
+    </div>
+  );
+}
