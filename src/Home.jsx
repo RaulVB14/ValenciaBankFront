63117cd5a95a2bfd9455dd445365a846338f0281
rrvb9 <rrvb9674@gmail.com>
2025-02-28 21:11:05 +0100

    cambios de paquetes

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import "./Home.css";
+import "./css/Home.css";
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
--- src/InicioSesion/Register.jsx
+++ src/InicioSesion/Register.jsx
@@ -1,4 +1,4 @@
-import "./Register.css";
+import "/src/css/Register.css";
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
